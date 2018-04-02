a5e5cbd94351f00885ad0d7526a428677cc78c8e
Alex Li <alexli@au1.ibm.com>
2018-04-02 22:09:24 +1000

    Check CQL keyspace existence before creating a new one.
    Signed-off-by: Alex Li <alexli@au1.ibm.com>

--- janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLStoreManager.java
+++ janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLStoreManager.java
@@ -252,21 +252,25 @@ public class CQLStoreManager extends DistributedStoreManager implements KeyColum
     }
 
     Session initializeSession(final String keyspaceName) {
-        final Configuration configuration = getStorageConfig();
-        final Map<String, Object> replication = Match(configuration.get(REPLICATION_STRATEGY)).of(
+        final Session s = this.cluster.connect();
+
+        if (this.cluster.getMetadata().getKeyspace(keyspaceName) == null) {
+            final Configuration configuration = getStorageConfig();
+            final Map<String, Object> replication = Match(configuration.get(REPLICATION_STRATEGY)).of(
                 Case($("SimpleStrategy"), strategy -> HashMap.<String, Object> of("class", strategy, "replication_factor", configuration.get(REPLICATION_FACTOR))),
                 Case($("NetworkTopologyStrategy"),
-                        strategy -> HashMap.<String, Object> of("class", strategy)
-                                .merge(Array.of(configuration.get(REPLICATION_OPTIONS))
-                                        .grouped(2)
-                                        .toMap(array -> Tuple.of(array.get(0), Integer.parseInt(array.get(1)))))))
+                    strategy -> HashMap.<String, Object> of("class", strategy)
+                        .merge(Array.of(configuration.get(REPLICATION_OPTIONS))
+                            .grouped(2)
+                            .toMap(array -> Tuple.of(array.get(0), Integer.parseInt(array.get(1)))))))
                 .toJavaMap();
 
-        final Session s = this.cluster.connect();
-        s.execute(createKeyspace(keyspaceName)
+            s.execute(createKeyspace(keyspaceName)
                 .ifNotExists()
                 .with()
                 .replication(replication));
+        }
+
         return s;
     }
 
